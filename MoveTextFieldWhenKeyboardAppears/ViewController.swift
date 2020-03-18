b178946f987ee7766ffd1ce1ff86b441ac3fd62a
Satnam <29satnam@gmail.com>
2020-03-18 16:29:38 +0530

    Swift 5 Update

--- MoveTextFieldWhenKeyboardAppears/ViewController.swift
+++ MoveTextFieldWhenKeyboardAppears/ViewController.swift
@@ -38,7 +38,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     }
     
     @objc func keyboardWillShow(notification:NSNotification){
-        var userInfo = notification.userInfo!
+        let userInfo = notification.userInfo!
         var keyboardFrame:CGRect = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
         keyboardFrame = self.view.convert(keyboardFrame, from: nil)
         
